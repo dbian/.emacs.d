9dc2d7616d0c8e34385957d595293d6751f8fe14
huadong bian <bianhuadong@live.com>
2022-12-30 22:24:31 +0800

    设置默认编码为utf-8，否则magit的编码总是错误的

--- init.el
+++ init.el
@@ -37,8 +37,8 @@
 
 
 
-;;(set-language-environment 'UTF-8)
-;;(set-locale-environment "UTF-8")
+(set-language-environment 'UTF-8)
+(set-locale-environment "UTF-8")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
