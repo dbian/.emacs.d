56373eb522b4757c052feb8273b7afcf02139450
dbian <zqqbhd@icloud.com>
2023-11-06 20:58:27 +0800

    2023-11-06 20:58:27

--- init.el
+++ init.el
@@ -80,6 +80,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:family "DejaVu Sans Mono" :foundry "PfEd" :slant normal :weight regular :height 98 :width normal))))
  '(org-document-title ((t ((\,@ headline) (\,@ variable-tuple) :height 2.0 :underline nil)))))
 
 ;; load my custom seperate init files
@@ -253,7 +254,7 @@
 
 
 ;; quick commit git
-(bind-key "C-c g" 'git-quick-commit)
+(bind-key "C-c <f11>" 'git-quick-commit)
 (bind-key "C-c <f12>" 'git-quick-pull)
 
 
