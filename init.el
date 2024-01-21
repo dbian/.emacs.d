502994b1476c070c3a7862ceebabc76b08c4fe38
Insight <bianhuadong@live.com>
2024-01-21 20:05:42 +0800

    2024-01-21 20:05:42

--- init.el
+++ init.el
@@ -55,42 +55,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(blink-cursor-mode nil)
- '(column-number-mode t)
- '(completion-auto-help t)
- '(current-language-environment "UTF-8")
- '(custom-enabled-themes '(solarized-gruvbox-dark))
- '(custom-safe-themes
-   '("efcecf09905ff85a7c80025551c657299a4d18c5fcfedd3b2f2b6287e4edd659" "fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c" "524fa911b70d6b94d71585c9f0c5966fe85fb3a9ddd635362bfabd1a7981a307" "3e200d49451ec4b8baa068c989e7fba2a97646091fd555eca0ee5a1386d56077" "51ec7bfa54adf5fff5d466248ea6431097f5a18224788d0bd7eb1257a4f7b773" "830877f4aab227556548dc0a28bf395d0abe0e3a0ab95455731c9ea5ab5fe4e1" "285d1bf306091644fb49993341e0ad8bafe57130d9981b680c1dbd974475c5c7" "00445e6f15d31e9afaa23ed0d765850e9cd5e929be5e8e63b114a3346236c44c" "833ddce3314a4e28411edf3c6efde468f6f2616fc31e17a62587d6a9255f4633" "d89e15a34261019eec9072575d8a924185c27d3da64899905f8548cbd9491a36" "4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" default))
- '(fringe-mode 0 nil (fringe))
- '(global-display-line-numbers-mode t)
- '(global-tab-line-mode t)
- '(indicate-empty-lines t)
- '(org-agenda-files '("~/ws/dev-diary"))
- '(org-capture-templates
-   '(("t" "all kinds of todos" entry
-      (file "~/ws/dev-diary/inbox.org")
-      "* TODO %? :: Captured @ %T%^{Effort|2d}p" :prepend t :jump-to-captured t)
-     ("c" "new comprehension on things" entry
-      (file "~/ws/dev-diary/comprehension.org")
-      "* %? :: added @ %T" :prepend t :jump-to-captured t)))
- '(org-confirm-babel-evaluate nil)
- '(package-selected-packages
-   '(gnuplot solarized-theme graphviz-dot-mode d2-mode rg exec-path-from-shell python-isort python-black python-pytest dired-sidebar elsa flymake-elsa lsp-pyright lsp-ui lsp-mode sideline-eldoc sideline eldoc-box racket-mode expand-region pet company-box git-gutter llama-cpp magit olivetti paredit v2ex-mode which-key cider geiser-chibi ace-window vertico orderless marginalia dumb-jump valign company tabby-mode))
  '(package-vc-selected-packages
-   '((sideline-eldoc :vc-backend Git :url "https://github.com/ginqi7/sideline-eldoc")
-     (vc-use-package :vc-backend Git :url "https://github.com/slotThe/vc-use-package")))
- '(recentf-exclude '(".*\\.gz" ".*\\.zip"))
- '(scroll-bar-mode nil)
- '(size-indication-mode t)
- '(tab-bar-history-mode t)
- '(tool-bar-mode nil))
+   '((tabby-mode :vc-backend Git :url "https://github.com/dbian/tabby-mode")
+     (vc-use-package :vc-backend Git :url "https://github.com/slotThe/vc-use-package"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(tab-line ((t (:inherit variable-pitch :background "grey85" :foreground "black" :height 1.1)))))
+ )
 
 ;; load my custom seperate init files
 (load  "completion")
