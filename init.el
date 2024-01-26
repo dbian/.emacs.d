3a1282a3fa63d094c7de55708f94eaac41a6a3c3
silohero <silo.mr.elephant@gmail.com>
2024-01-26 10:15:47 +0800

    2024-01-26 10:15:47

--- init.el
+++ init.el
@@ -121,8 +121,9 @@
 (use-package golden-ratio
   :config
   (golden-ratio-mode 1)
+  (golden-ratio-toggle-widescreen)
   (setq golden-ratio-adjust-factor .8
-      golden-ratio-wide-adjust-factor .8)
+	golden-ratio-wide-adjust-factor .8)
   )
 
 (use-package olivetti
