f15626a553acd6fcbb200913cf9082dc696d5ade
silohero <silo.mr.elephant@gmail.com>
2024-02-23 15:16:57 +0800

    2024-02-23 15:16:57

--- boxes/init-font.el
+++ boxes/init-font.el
@@ -29,16 +29,16 @@
 
 ;; (set-face-attribute 'default nil :height 110)
 
-(defun max/set-font (FONT-NAME CN-FONT-NAME &optional INITIAL-SIZE CN-FONT-RESCALE-RATIO)
-  "Set different font-family for Latin and Chinese charactors."
-  (let* ((size (or INITIAL-SIZE 14))
-	 (ratio (or CN-FONT-RESCALE-RATIO 0.0))
-	 (main (font-spec :name FONT-NAME :size size))
-	 (cn (font-spec :name CN-FONT-NAME)))
-    (set-face-attribute 'default nil :font main)
-    (dolist (charset '(kana han symbol cjk-misc bopomofo))
-      (set-fontset-font t charset cn))
-    (setq face-font-rescale-alist (if (/= ratio 0.0) `((,CN-FONT-NAME . ,ratio)) nil))))
+;; (defun max/set-font (FONT-NAME CN-FONT-NAME &optional INITIAL-SIZE CN-FONT-RESCALE-RATIO)
+;;   "Set different font-family for Latin and Chinese charactors."
+;;   (let* ((size (or INITIAL-SIZE 14))
+;; 	 (ratio (or CN-FONT-RESCALE-RATIO 0.0))
+;; 	 (main (font-spec :name FONT-NAME :size size))
+;; 	 (cn (font-spec :name CN-FONT-NAME)))
+;;     (set-face-attribute 'default nil :font main)
+;;     (dolist (charset '(kana han symbol cjk-misc bopomofo))
+;;       (set-fontset-font t charset cn))
+;;     (setq face-font-rescale-alist (if (/= ratio 0.0) `((,CN-FONT-NAME . ,ratio)) nil))))
 
 ;; 你好，世界
 ;; abcdefgh
